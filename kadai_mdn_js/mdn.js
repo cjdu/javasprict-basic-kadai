1222e3ba8323d8003c2055eca08d8be09c500cf0
cjdu <jssdbo1277@yahoo.co.jp>
2024-05-05 19:59:30 +0900

    最初のコミット

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,1 +1,8 @@
+const date = () => {
 
+const date = new Date ('9 22, 2023');
+const date1 = date.getDate();
+console.log(date.getFullYear() + '年' + date.getMonth() + '月' + date1 + '日'); 
+}
+
+date();
