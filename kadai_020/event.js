43828468606aca9164f5477f804b49d605a16828
cjdu <jssdbo1277@yahoo.co.jp>
2024-05-11 17:18:47 +0900

    ３回目のコミット

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,11 +1,11 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
-const Btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 
 // textというidを持つHTML要素を取得し、定数に代入する
-const Text = document.getElementById('text');
+const text = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
-Btn.addEventListener('click', () => {
+btn.addEventListener('click', () => {
   
-  document.getElementById('text').textContent = 'ボタンをクリックしました';
+ text.textContent = 'ボタンをクリックしました';
 });
